6e5d654c475a7c3f84b67e45ae7846971768bcd8
Neuromantic <git@neuromantic.com>
2012-09-14 20:27:32 -0400

    add socket.io (again?)

--- src/com/neuromantic/arete/Arete.js
+++ src/com/neuromantic/arete/Arete.js
@@ -15,7 +15,7 @@ _package( 'com.neuromantic.arete',
 	_class( 'Arete' )._extends( 'Server', {
 		Arete : function(){
 			var server = new Server( { host: '127.0.0.1', port: 2207 } );
-			var sockets = new Sockets( { http: server } );
+			var sockets = new Sockets( {http: server._.http});
 			var logger = new Logger();
 			var app = new Apps();
 			var files = new Static( { root: '/' }  );
